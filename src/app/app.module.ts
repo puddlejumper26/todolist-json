b5c7d0e80e3f441d6403a94ef430a75e2322afc7
puddlejumper26 <xiang.wu@hotmail.de>
2020-06-18 20:04:05 +0800

    add dashboard, item, edit-modal components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditModalComponent } from './components/edit-modal/edit-modal.component';
+import { ItemComponent } from './components/item/item.component';
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EditModalComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule
